88de6c1c6fd9bb0aed0740ffa53191061df0a3be
Eric Struhl <6519009+lotap@users.noreply.github.com>
2022-05-24 03:02:27 -0500

    hell yeeah

--- frontend/src/routes/[name].svelte
+++ frontend/src/routes/[name].svelte
@@ -40,9 +40,72 @@
 	};
 </script>
 
-<h1>Hey {name}!</h1>
-<h2>Get ready to mint your Chunkz!</h2>
-<h3>Your address is: {$signerAddress}</h3>
+<style>
+	.nifty-logo {
+			width: 80px;
+	}
+	.traits-area {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+	}
+	.trait {
+			margin: 0px 20px;
+	}
+	.trait span {
+			display: inline-block;
+			color: #3E4353;
+			margin-bottom: 8px;
+	}
+	.trait-viewer {
+			width: 250px;
+			height: 350px;
+			border: 2px solid #fff;
+			border-radius: 10px;
+	}
+	.btn{
+			background-color: #E87AFF;
+			color: #fff;
+			border-radius: calc(97px / 2);
+			height: 48px;
+			padding: 0px 24px;
+			border: none;
+			display: inline-block;
+			margin-bottom: 40px;
+	}
+	.btn:hover {
+			background-color: #CF6CE4;
+			cursor: pointer;
+	}
+</style>
+	<img class="nifty-logo" src="../static/nifty-logo.png">
+	<h1>Chunkz</h1>
+
+	<div class="traits-area">
+			<div class="trait">
+					<span>Hat</span>
+					<div class="trait-viewer"></div>
+			</div>
+			<div class="trait">
+					<span>Eyes</span>
+					<div class="trait-viewer"></div>
+			</div>
+			<div class="trait">
+					<span>Ears</span>
+					<div class="trait-viewer"></div>
+			</div>
+			<div class="trait">
+					<span>Nose</span>
+					<div class="trait-viewer"></div>
+			</div>
+			<div class="trait">
+					<span>Mouth</span>
+					<div class="trait-viewer"></div>
+			</div>
+	</div>
+
+	<h2>Hey {name}!</h2>
+	<h3>Get in loser, we're minting Chunkz!</h3>
 
 {#if status === 'pendingMint'}
 <p>Rolling...</p>
@@ -50,11 +113,15 @@
 
 {#if status === 'uninitialized'}
 <button
+class="mintbtn btn"
 	on:click={(_) => {
-		mintNow();
-	}}>Mint now!</button
+			mintNow();
+	}}>Mint your Chunk</button
 >
 {:else if status === 'success'}
-	<button>Next</button>
+	<button class="btn">Next</button>
 {/if}
+
+<h3 style="margin-bottom: 16px;">Wallet Address</h3>
+<p style="margin:0px;">{$signerAddress}</p>
 
