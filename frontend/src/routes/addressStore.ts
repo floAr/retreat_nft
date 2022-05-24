b0d85ab43ee14a38285067b5ac42bfa9fd7f112e
Florian Uhde <florian.uhde@posteo.de>
2022-05-23 19:47:26 -0500

    store for getting the addresses from

--- frontend/src/routes/addressStore.ts
+++ frontend/src/routes/addressStore.ts
@@ -1,16 +1,45 @@
 import { writable } from 'svelte/store';
 export interface Entry {
-    name:string
-    address:string
+    name: string
+    address: string
 }
+export const getNext = () => {
+    // retrieve one entry from Addresses and remove it
+    let entry: Entry  = {
+        name: '',
+        address: ''
+    }
+
+    Addresses.update(data => {
+        // get random entry
+        if (data.length > 0) {
+            const index = Math.floor(Math.random() * data.length);
+            entry = data[index];
+            // remove entry
+            data.splice(index, 1);
+        }
+        return data;
+    })
+    return entry;
+}
+
 
 export const Addresses = writable<Entry[]>([
     {
-        name: 'floAr',
-        address: 'abcd'
+        name: '1',
+        address: '1'
     },
     {
-        name: 'dfasfaf',
-        address: 'abcd'
+        name: '2',
+        address: '2'
     },
+    {
+        name: '3',
+        address: '3'
+    },
+    {
+        name: '4',
+        address: '4'
+    },
+
 ]); // we put addresses here
